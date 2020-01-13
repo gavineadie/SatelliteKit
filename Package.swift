61a5bf479072b6b849c316dcffda858501d201f7
Gavin Eadie <gavin@umich.edu>
2020-01-13 00:21:26 -0500

    don't make library dynamic ..

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SatelliteKit",
     products: [
-        .library(name: "SatelliteKit", type: .dynamic, targets: ["SatelliteKit"]),
+        .library(name: "SatelliteKit", targets: ["SatelliteKit"]),
     ],
     targets: [
         .target(name: "SatelliteKit", dependencies: []),
