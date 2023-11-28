dfaf1099edd18564749cf9a4210fa736cf5e8ab6
Gavin Eadie <gavin@umich.edu>
2023-11-27 20:38:18 -0500

    add .visionOS(.v1) ..

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SatelliteKit",
     platforms: [
-        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4)
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4), .visionOS(.v1)
     ],
     products: [
         .library(
