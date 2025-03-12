167dc4927a8bf74e458a9d45e878319435f2462c
Mathis Gaignet <m.gaignet@yahoo.fr>
2025-03-12 16:33:22 +0100

    new test

--- Tests/SatelliteKitTests/ShadowEventsTests.swift
+++ Tests/SatelliteKitTests/ShadowEventsTests.swift
@@ -5,24 +5,26 @@ import Foundation
 
 struct Test {
 
+    
+    // Old test results may be outdated
     @Test
     func calculateShadowEventsTest_ShouldReturnCorrectTime() async throws {
         let TLE = (
             line0: "",
-            line1: "1 25544U 98067A   25069.48991731  .00012244  00000-0  22315-3 0  9993",
-            line2: "2 25544  51.6366  75.8722 0006264   7.6024  91.8047 15.49864464499852"
+            line1: "1 25544U 98067A   25071.47094811  .00014495  00000-0  26208-3 0  9993",
+            line2: "2 25544  51.6351  66.0638 0006419  13.0256 347.0898 15.49921961500167"
         )
         let elements = try Elements(TLE.0, TLE.1, TLE.2)
         let satellite = Satellite(elements: elements)
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-        dateFormatter.timeZone = TimeZone(identifier: "America/Los_Angeles")
+        dateFormatter.timeZone = TimeZone(identifier: "Europe/Paris")
 
-        let riseTime = dateFormatter.date(from: "2025-03-10T19:50:00")!
-        let setTime = dateFormatter.date(from: "2025-03-10T20:02:00")!
+        let riseTime = dateFormatter.date(from: "2025-03-21T20:58:00")!
+        let setTime = dateFormatter.date(from: "2025-03-21T21:07:00")!
         
-        let observer = LatLonAlt(34.0537, -118.2428, 0)
+        let observer = LatLonAlt(48.8589, 2.32, 0)
         
         let shadowTime = try await ShadowEvents.calculateShadowEvents(
             satellite: satellite,
@@ -31,8 +33,8 @@ struct Test {
             observer: observer
         )
         
-        let expectedMin = dateFormatter.date(from: "2025-03-10T19:59:00")!
-        let expectedMax = dateFormatter.date(from: "2025-03-10T20:00:00")!
+        let expectedMin = dateFormatter.date(from: "2025-03-21T21:05:00")!
+        let expectedMax = dateFormatter.date(from: "2025-03-21T21:06:00")!
         
         print("shadow time is \(shadowTime)", terminator: "\n")
         
